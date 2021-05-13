1ce789605e1d5dbf796ad3e4adc0231175957996
karen1993t <avtandilyan1993t@gmail.com>
2021-05-13 13:41:13 +0400

    newMenu

--- app/src/main/java/com/electrical/myapplication/MainActivity.kt
+++ app/src/main/java/com/electrical/myapplication/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.electrical.myapplication
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+
+    // Create Menu Bar..
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_items,menu)
+        return true
+    }
+    @SuppressLint("QueryPermissionsNeeded")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_send_to_mail -> {
+                val intentMail = Intent(Intent.ACTION_SEND).apply {
+                    Uri.parse("mailto:")
+                    putExtra(
+                        Intent.EXTRA_EMAIL, resources
+                        .getStringArray(R.array.mail_address_programmers))
+                    type = "message/rfc822"
+                }
+                if (intentMail.resolveActivity(packageManager) != null){
+                    startActivity(intentMail)
+                }else{
+                    Toast.makeText(this,R.string.errorSend, Toast.LENGTH_SHORT).show()
+                }
+            }
+            R.id.menu_share_app -> {
+                //  val intentSend = Intent(Intent.ACTION_SEND)
+                //  startActivity(intentSend)
+            }
+            R.id.menu_like -> {
+                //
+            }
+            R.id.menu_settings -> {
+            }
+        }
+        return true
     }
 }
