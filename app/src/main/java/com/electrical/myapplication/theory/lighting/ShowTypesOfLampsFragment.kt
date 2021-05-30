f620b93fa5480bfdfc929da7fe27cf42d6143f26
karen1993t <avtandilyan1993t@gmail.com>
2021-05-31 00:02:02 +0400

    31.05.21

--- app/src/main/java/com/electrical/myapplication/theory/lighting/ShowTypesOfLampsFragment.kt
+++ app/src/main/java/com/electrical/myapplication/theory/lighting/ShowTypesOfLampsFragment.kt
@@ -15,6 +15,12 @@ class ShowTypesOfLampsFragment : Fragment() {
     private var countClick2 = 0
     private var countClick3 = 0
     private var countClick4 = 0
+    private var countClick5 = 0
+    private var countClick6 = 0
+    private var countClick7 = 0
+    private var countClick8 = 0
+    private var countClick9 = 0
+    private var countClick10 = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -87,6 +93,78 @@ class ShowTypesOfLampsFragment : Fragment() {
                 countClick4++
                 bindingTypesLamps.descriptionLightingTypesOfLamps4.text = ""
                 bindingTypesLamps.iconDescriptionUpDown4.setBackgroundResource(R.drawable.ic_info_sockets_plugs_down)
+            }
+        }
+        bindingTypesLamps.layoutDescriptionLightingTypesOfLamps5.setOnClickListener {
+            if (countClick5 % 2 == 0) {
+                countClick5++
+                bindingTypesLamps.iconDescriptionUpDown5.setBackgroundResource(R.drawable.ic_info_sockets_plugs_up)
+                bindingTypesLamps.descriptionLightingTypesOfLamps5.text =
+                    getString(R.string.info_description_lighting_types_of_lamps_5)
+            } else {
+                countClick5++
+                bindingTypesLamps.descriptionLightingTypesOfLamps5.text = ""
+                bindingTypesLamps.iconDescriptionUpDown5.setBackgroundResource(R.drawable.ic_info_sockets_plugs_down)
+            }
+        }
+        bindingTypesLamps.layoutDescriptionLightingTypesOfLamps6.setOnClickListener {
+            if (countClick6 % 2 == 0) {
+                countClick6++
+                bindingTypesLamps.iconDescriptionUpDown6.setBackgroundResource(R.drawable.ic_info_sockets_plugs_up)
+                bindingTypesLamps.descriptionLightingTypesOfLamps6.text =
+                    getString(R.string.info_description_lighting_types_of_lamps_6)
+            } else {
+                countClick6++
+                bindingTypesLamps.descriptionLightingTypesOfLamps6.text = ""
+                bindingTypesLamps.iconDescriptionUpDown6.setBackgroundResource(R.drawable.ic_info_sockets_plugs_down)
+            }
+        }
+        bindingTypesLamps.layoutDescriptionLightingTypesOfLamps7.setOnClickListener {
+            if (countClick7 % 2 == 0) {
+                countClick7++
+                bindingTypesLamps.iconDescriptionUpDown7.setBackgroundResource(R.drawable.ic_info_sockets_plugs_up)
+                bindingTypesLamps.descriptionLightingTypesOfLamps7.text =
+                    getString(R.string.info_description_lighting_types_of_lamps_7)
+            } else {
+                countClick7++
+                bindingTypesLamps.descriptionLightingTypesOfLamps7.text = ""
+                bindingTypesLamps.iconDescriptionUpDown7.setBackgroundResource(R.drawable.ic_info_sockets_plugs_down)
+            }
+        }
+        bindingTypesLamps.layoutDescriptionLightingTypesOfLamps8.setOnClickListener {
+            if (countClick8 % 2 == 0) {
+                countClick8++
+                bindingTypesLamps.iconDescriptionUpDown8.setBackgroundResource(R.drawable.ic_info_sockets_plugs_up)
+                bindingTypesLamps.descriptionLightingTypesOfLamps8.text =
+                    getString(R.string.info_description_lighting_types_of_lamps_8)
+            } else {
+                countClick8++
+                bindingTypesLamps.descriptionLightingTypesOfLamps8.text = ""
+                bindingTypesLamps.iconDescriptionUpDown8.setBackgroundResource(R.drawable.ic_info_sockets_plugs_down)
+            }
+        }
+        bindingTypesLamps.layoutDescriptionLightingTypesOfLamps9.setOnClickListener {
+            if (countClick9 % 2 == 0) {
+                countClick9++
+                bindingTypesLamps.iconDescriptionUpDown9.setBackgroundResource(R.drawable.ic_info_sockets_plugs_up)
+                bindingTypesLamps.descriptionLightingTypesOfLamps9.text =
+                    getString(R.string.info_description_lighting_types_of_lamps_9)
+            } else {
+                countClick9++
+                bindingTypesLamps.descriptionLightingTypesOfLamps9.text = ""
+                bindingTypesLamps.iconDescriptionUpDown9.setBackgroundResource(R.drawable.ic_info_sockets_plugs_down)
+            }
+        }
+        bindingTypesLamps.layoutDescriptionLightingTypesOfLamps10.setOnClickListener {
+            if (countClick10 % 2 == 0) {
+                countClick10++
+                bindingTypesLamps.iconDescriptionUpDown10.setBackgroundResource(R.drawable.ic_info_sockets_plugs_up)
+                bindingTypesLamps.descriptionLightingTypesOfLamps10.text =
+                    getString(R.string.info_description_lighting_types_of_lamps_10)
+            } else {
+                countClick10++
+                bindingTypesLamps.descriptionLightingTypesOfLamps10.text = ""
+                bindingTypesLamps.iconDescriptionUpDown10.setBackgroundResource(R.drawable.ic_info_sockets_plugs_down)
             }
         }
     }
